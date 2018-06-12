8aee19413b6225c28327ba4500bc7d903687d914
liushaozhen <804199994@qq.com>
2018-06-12 19:43:42 +0800

    路由修改

--- src/modules/sys-manage/router.jsx
+++ src/modules/sys-manage/router.jsx
@@ -4,7 +4,7 @@ import { userList } from "./user-manage/containers";
 import PlanIndexProj from "./person-manage/containers";
 const Routers = ({ match }) => (
   <div>
-    <Route exact path={`${match.url}/list`} component={userList} />
+    <Route exact path={`${match.url}/userlist`} component={userList} />
     <Route
       exact
       path={`${match.url}/person-manage`}
