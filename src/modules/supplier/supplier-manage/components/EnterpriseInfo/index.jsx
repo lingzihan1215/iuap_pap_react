7071c85e8cf07418818016830536024ab427f863
liuzhipeng <liuzhpc@yonyou.com>
2018-06-14 10:20:02 +0800

    package.json中添加bee-city-select

--- src/modules/supplier/supplier-manage/components/EnterpriseInfo/index.jsx
+++ src/modules/supplier/supplier-manage/components/EnterpriseInfo/index.jsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import {Radio,Row,Col,FormControl,Label,Button,Icon} from 'tinper-bee';
+import mirror, { actions, connect } from "mirrorx";
 import Form from 'bee-form';
 import Select from 'bee-select';
+import createModal from 'yyuap-ref';
 
 import './index.less';
 const Option = Select.Option;
@@ -34,7 +36,47 @@ class EnterpriseInfo extends Component {
     onRefClick = (inputid,propForm)=>{
         // 弹出参照
         return ()=>{
-            alert("弹出参照");
+            
+            let option = {
+                title: '参照类型',
+                refType:2,//1:树形 2.单表 3.树卡型 4.多选 5.default
+                isRadio:false,//1.true 单选 2.false多选
+                hasPage:true,
+                tabData:[//tab标签
+                    {"title":"常用","key":"commonUse"},
+                    {"title":"全部","key":"total"},
+                    {"title":"推荐","key":"recommed"}
+                ],// option中可增加defaultActiveKey作为默认tab标签
+                param:{//url请求参数
+                    refCode:'newuser',
+                    tenantId:'',
+                    sysId:'',
+                },
+                refModelUrl:{
+                    TableBodyUrl:'/newref/rest/iref_ctr/blobRefTreeGrid',//表体请求
+                    TableBarUrl:'/newref/rest/iref_ctr/refInfo',//表头请求
+                },
+                checkedArray:[],
+                onCancel: function (p) {
+                    console.log(p)
+                },
+                onSave: function (sels) {
+                  console.log("sels",sels,inputid);
+                    let param = {
+                        inputid:inputid,
+                        singleRefData:sels
+                    }
+                    // actions.master.setInputFromRef(param);
+                    let {setFieldsValue} = propForm;
+                    console.log(sels[0]["refname"]);
+                    let obj={};
+                    obj[inputid+""]=sels[0]["refname"];  
+                    console.log(obj)
+                    setFieldsValue(obj);
+                },
+            }
+            createModal(option);
+               
         }
     }
 
@@ -50,6 +92,7 @@ class EnterpriseInfo extends Component {
     }
     render() {
         const { getFieldProps, getFieldError,getFieldDecorator} = this.props.form;
+        console.log("this.props.form",this.props.form);
         return (
             <div className="supplier-page">
                 <div className="supplier-user-head">企业信息:</div>
@@ -574,7 +617,7 @@ class EnterpriseInfo extends Component {
                                                 })
                                             }
                                         />
-                                        <Icon type="uf-navmenu" key={"icon"} className="reficon_adjust" onClick={this.onRefClick("depositbank", this.props.form)}></Icon>
+                                        <Icon type="uf-navmenu" key={"icon"} className="reficon_adjust" onClick={this.onRefClick("banktype", this.props.form)}></Icon>
                                         <span className='error'>
                                             {getFieldError('banktype')}
                                         </span>
