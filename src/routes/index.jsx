d2cc586be889c1820145775c115d663b8cf7c238
guoyff <guoyff@yonyou.com>
2018-06-14 14:19:29 +0800

    update routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,7 +1,7 @@
 /*
 * 路由表
 * */
-import React from "react";
+import React, { Component } from "react";
 import { Router, Route } from "mirrorx";
 import order from "modules/order/router";
 import Supplier from "modules/supplier/router";
@@ -10,24 +10,27 @@ import MainLayout from "layout";
 import system from 'modules/sys-manage/router';
 import "./index.less";
 
-console.log(__MODE__)
+const MainRoutes = () => (
+  <div>
+    <Route path="/order" component={order} />
+    <Route path="/supplier" component={Supplier} />
+    <Route path="/contract" component={contracts} />
+    <Route path="/system" component={system} />
+  </div>
+)
 
-const App = () => (
-  <Router>
-    <div>
-      <MainLayout />
-      <div className="layout-content">
-        {/* 订单管理 */}
-        <Route path="/order" component={order} />
-        {/* 供应商 */}
-        <Route path="/supplier" component={Supplier} />
-        {/* 合同管理 */}
-        <Route path="/contract" component={contracts} />
-        {/* 系统管理 */}
-        <Route path="/system" component={system} />
-      </div>
-    </div>
-  </Router>
-);
+class App extends Component {
+  render(){
+    return (
+      <Router>
+        <div className="route-content">
+          { (__MODE__ == "development") ? <MainLayout /> : "" }
+          { (__MODE__ == "development") ? <div className="layout-content"><MainRoutes /></div> : <MainRoutes/> }
+        </div>
+      </Router>
+    )
+  }
+}
+
 
 export default App;
