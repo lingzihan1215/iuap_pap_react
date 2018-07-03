18500703670783241896c747ebdceb5252ef43a7
liuzhipeng <liuzhpc@yonyou.com>
2018-07-03 15:20:11 +0800

    删除resource、interimplain

--- src/modules/examples/ref-example/components/Ref/index.jsx
+++ src/modules/examples/ref-example/components/Ref/index.jsx
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { actions } from "mirrorx";
-import { Label,FormControl,Icon,Button,Table,Checkbox } from 'tinper-bee';
+import { Label,FormControl,Icon,Button,Table,Checkbox,Row,Col } from 'tinper-bee';
 import { Scrollbars } from 'react-custom-scrollbars';
 import Header from 'components/Header';
 import multiSelect from "tinper-bee/lib/multiSelect.js";
-const MultiSelectTable = multiSelect(Table, Checkbox);
+import createModal from 'yyuap-ref';
+import moment from "moment";
+import Form from 'bee-form';
 
 import './index.less'
+
+const MultiSelectTable = multiSelect(Table, Checkbox);
+const FormItem = Form.FormItem;
+
 
 class Ref extends Component {
     constructor(props) {
@@ -16,12 +22,43 @@ class Ref extends Component {
         };
     }
 
-    onAdd = ()=> {
+    onRefClick = (inputid,propForm)=> {
+        return ()=>{
+            console.log("inputid",inputid);
+            let refType;
+            if(inputid=="searchref"){
+                // 带搜索框的列表
+                refType = 6
+            }else {
+                // 穿梭框
+                refType = 5;
+            }
+            let option = {
+                refType:refType,//5 穿梭框; 6 多过滤项
+                className:'',
+                param:{//url请求参数
+                    refCode:'common_ref',
+                    tenantId:'',
+                    sysId:'',
+                    transmitParam:'EXAMPLE_CONTACTS,EXAMPLE_ORGANIZATION',//传后台表名
+                },
+                refModelUrl:{
+                    TableBodyUrl:'/newref/rest/iref_ctr/blobRefTreeGrid',//表体请求
+                    TableBarUrl:'/newref/rest/iref_ctr/refInfo',//表头请求
+                },
+                checkedArray: [],
+                onSave: function (sels) {
+                  console.log(sels);
+                },
+                filterKey:[
+                    {title:'人员名称',key:'peoname'},
+                    {title:'人员编码',key:'peocode'},
+                ],//传过滤的字段和字段值
+                
+            }
 
-    }
-
-    tabelSelect = (data)=>{
-        console.log(data);
+            createModal(option);
+        }
     }
     render() {
         const column = [
@@ -95,23 +132,43 @@ class Ref extends Component {
             
         ];
         let {showLoading } = this.state;
+        const { getFieldProps, getFieldError,getFieldDecorator} = this.props.form;
         return (
             <div className="ref-page">
                 <Header title='参照示例'/>
                 <div>
-                    <Button className="editable-add-btn" colors="primary" onClick={this.onAdd} style={{ marginLeft: "5px" }} >新增</Button>
-                    <div className="mt10 b-shadow">
-                        {/* <Scrollbars> */}
-                            <MultiSelectTable
-                                scroll = {{x:1250,y:500}}
-                                loading={{ show: showLoading, loadingType: "line" }}
-                                rowKey={(r, i) => i}
-                                columns={column}
-                                data={[]}
-                                multiSelect={{ type: "checkbox" }}
-                                getSelectedDataFunc={this.tabelSelect}
-                            />
-                        {/* </Scrollbars> */}
+                    {/* <Button className="editable-add-btn" colors="primary" onClick={this.onAdd} style={{ marginLeft: "5px" }} >新增</Button> */}
+                    <div className="mt10">
+                        
+                        <Row>
+                            <Col md={4} xs={6} className="mt10">
+                                <FormItem>
+                                    <Label>搜索参照:</Label>
+                                    <FormControl className='form-item'
+                                        {
+                                        ...getFieldProps('searchref', {
+                                            initialValue: '',
+                                        })
+                                        }
+                                    />
+                                    <Icon type="uf-navmenu" key={"icon"} className="reficon_adjust" onClick={this.onRefClick("searchref",this.props.form)}></Icon>
+                                </FormItem>
+                            </Col>
+
+                            <Col md={4} xs={6} className="mt10">
+                                <FormItem>
+                                    <Label>穿梭框:</Label>
+                                    <FormControl className='form-item'
+                                        {
+                                        ...getFieldProps('shuttlebox', {
+                                            initialValue: '',
+                                        })
+                                        }
+                                    />
+                                    <Icon type="uf-navmenu" key={"icon"} className="reficon_adjust" onClick={this.onRefClick("shuttlebox",this.props.form)}></Icon>
+                                </FormItem>
+                            </Col>
+                        </Row>
                     </div>
                 </div>
             </div>
@@ -119,4 +176,4 @@ class Ref extends Component {
     }
 }
 
-export default Ref;
+export default Form.createForm()(Ref);
