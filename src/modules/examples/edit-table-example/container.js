6f8e02551555c1fdaab1185d68885f24de059632
liushaozhen <804199994@qq.com>
2018-07-10 09:20:10 +0800

    padding

--- src/modules/examples/edit-table-example/container.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import mirror, { connect } from 'mirrorx';
-import EditTable from './components';//多选table
-
-import model from './model'
-
-mirror.model(model);
-
-export const ConnectedEditTable2 = connect( state => state.editTable2, null )(EditTable);
