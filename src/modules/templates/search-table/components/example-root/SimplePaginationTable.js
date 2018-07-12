2715a81d2c893a41d6afb96eddcf866c0cfcd843
kvkens <kvkens@qq.com>
2018-07-12 11:35:09 +0800

    add:按钮权限Auth

--- src/modules/templates/search-table/components/example-root/SimplePaginationTable.js
+++ src/modules/templates/search-table/components/example-root/SimplePaginationTable.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react'
 import PaginationTable from 'components/PaginationTable'
-import {BpmButtonSubmit,BpmButtonRecall} from 'yyuap-bpm';
+import { BpmButtonSubmit, BpmButtonRecall } from 'yyuap-bpm';
 import { actions } from 'mirrorx';
-import { Button,Message } from 'tinper-bee';
+import { Button, Message } from 'tinper-bee';
 import moment from "moment/moment";
 import Header from 'components/Header';
 import ExampleForm from '../example-form';
 
 export default class SimplePaginationTable extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        let self=this;
+        let self = this;
         this.state = {
             // 表格中所选中的数据，拿到后可以去进行增删改查
             selectData: [],
             step: 10,
-            column:[
+            column: [
                 {
                     title: "序号",
                     dataIndex: "index",
@@ -30,8 +30,8 @@ export default class SimplePaginationTable extends Component {
                     dataIndex: "orderCode",
                     key: "orderCode",
                     width: 250,
-                    className:"td-detail",
-                    onCellClick: (record) => this.cellClick(record,2)
+                    className: "td-detail",
+                    onCellClick: (record) => this.cellClick(record, 2)
                 },
                 {
                     title: "供应商名称",
@@ -85,13 +85,13 @@ export default class SimplePaginationTable extends Component {
                     title: "操作",
                     dataIndex: "d",
                     key: "d",
-                    width:100,
+                    width: 100,
                     fixed: "right",
                     render(text, record, index) {
                         return (
                             <div className='operation-btn'>
-                                <i size='sm' className='uf uf-search edit-btn' onClick={() => { self.cellClick(record,2) }}></i>
-                                <i size='sm' className='uf uf-pencil edit-btn' onClick={() => { self.cellClick(record,1) }}></i>
+                                <i size='sm' className='uf uf-search edit-btn' onClick={() => { self.cellClick(record, 2) }}></i>
+                                <i size='sm' className='uf uf-pencil edit-btn' onClick={() => { self.cellClick(record, 1) }}></i>
                                 <i size='sm' className='uf uf-del del-btn' onClick={() => { self.delItem(record, index) }}></i>
                             </div>
                         )
@@ -100,9 +100,10 @@ export default class SimplePaginationTable extends Component {
             ]
         }
     }
-    componentDidMount(){
+    componentDidMount() {
         // this.setState({ step: this.props.pageSize })
         actions.searchTable.loadList();//table数据
+        actions.searchTable.queryAuth('sany_order');//加载按钮权限
     }
     tabelSelect = (data) => {//tabel选中数据
         this.setState({
@@ -113,19 +114,19 @@ export default class SimplePaginationTable extends Component {
      * 编辑,详情，增加
      */
 
-    cellClick = async (record,btnFlag) => {
+    cellClick = async (record, btnFlag) => {
         await actions.searchTable.updateState({
-            rowData : record,
+            rowData: record,
         });
 
         let id = "";
-        if(record){
+        if (record) {
             id = record["id"];
         }
         actions.routing.push(
             {
                 pathname: 'example-edit',
-                search:`?search_id=${id}&btnFlag=${btnFlag}`
+                search: `?search_id=${id}&btnFlag=${btnFlag}`
             }
         )
     }
@@ -152,45 +153,45 @@ export default class SimplePaginationTable extends Component {
         })
     }
 
-    onSubmitSuc = async ()=>{
+    onSubmitSuc = async () => {
         await actions.searchTable.loadList();
-        actions.searchTable.updateState({showLine:false});
-        Message.create({content: '单据提交成功', color: 'success'});
+        actions.searchTable.updateState({ showLine: false });
+        Message.create({ content: '单据提交成功', color: 'success' });
 
     }
     // 提交操作初始执行操作
-    onSubmitStart = ()=>{
-        actions.searchTable.updateState({showLine:true});
+    onSubmitStart = () => {
+        actions.searchTable.updateState({ showLine: true });
 
     }
     // 提交失败回调函数
-    onSubmitFail = (error)=>{
-        actions.searchTable.updateState({showLine:false});
-        Message.create({content: error.msg, color: 'danger'});
+    onSubmitFail = (error) => {
+        actions.searchTable.updateState({ showLine: false });
+        Message.create({ content: error.msg, color: 'danger' });
 
     }
 
     // 撤回成功，失败，开始回调函数
-    onRecallSuc = async ()=>{
+    onRecallSuc = async () => {
         console.log("onRecallSuc 成功进入recall回调");
         await actions.searchTable.loadList();
-        actions.searchTable.updateState({showLine:false});
-        Message.create({content: '单据撤回成功', color: 'success'});
+        actions.searchTable.updateState({ showLine: false });
+        Message.create({ content: '单据撤回成功', color: 'success' });
 
     }
-    onRecallFail = (error)=>{
-        actions.searchTable.updateState({showLine:false});
-        Message.create({content: error.msg, color: 'danger'});
+    onRecallFail = (error) => {
+        actions.searchTable.updateState({ showLine: false });
+        Message.create({ content: error.msg, color: 'danger' });
 
     }
-    onRecallStart = ()=>{
-        actions.searchTable.updateState({showLine:true});
+    onRecallStart = () => {
+        actions.searchTable.updateState({ showLine: true });
     }
 
     //查看方法
-    onExamine = async (text, record, index)=> {
+    onExamine = async (text, record, index) => {
         console.log("record", record);
-        await actions.searchTable.updateState({rowData:record});
+        await actions.searchTable.updateState({ rowData: record });
         await actions.routing.push(
             {
                 pathname: 'example-edit',
@@ -200,45 +201,45 @@ export default class SimplePaginationTable extends Component {
     }
 
     // 清空selectData
-    clearSelData = ()=>{
+    clearSelData = () => {
         this.setState({
-            selectData:[]
+            selectData: []
         })
     }
 
-    render(){
-        const self=this;
-        let { list, showLoading, pageIndex, pageSize, totalPages } = this.props;
-        let {selectData} = this.state;
+    render() {
+        const self = this;
+        let { addAuth, submitAuth, recallAuth, list, showLoading, pageIndex, pageSize, totalPages } = this.props;
+        let { selectData } = this.state;
         // console.log("selectData",selectData)
         // console.log("${GROBAL_HTTP_CTX}",`${GROBAL_HTTP_CTX}`);
-        console.log("list",list)
+        console.log("list", list)
         return (
             <div className='example-root'>
                 <Header title='简单分页表格示例' back={true} />
-                <ExampleForm { ...this.props }/>
+                <ExampleForm {...this.props} />
                 <div className='table-header'>
-                    <Button size='sm' shape="border" onClick={() => { self.cellClick({},0) }}>
+                    {addAuth && <Button size='sm' shape="border" onClick={() => { self.cellClick({}, 0) }}>
                         新增
-                    </Button>
-                    <BpmButtonSubmit
+                    </Button>}
+                    {submitAuth && <BpmButtonSubmit
                         className="ml5 "
-                        checkedArray = {selectData}
-                        funccode = "react"
-                        nodekey = "003"
-                        url = {`${GROBAL_HTTP_CTX}/sany_order/submit`}
-                        onSuccess = {this.onSubmitSuc}
-                        onError = {this.onSubmitFail}
+                        checkedArray={selectData}
+                        funccode="react"
+                        nodekey="003"
+                        url={`${GROBAL_HTTP_CTX}/sany_order/submit`}
+                        onSuccess={this.onSubmitSuc}
+                        onError={this.onSubmitFail}
                         onStart={this.onSubmitStart}
-                    />
-                    <BpmButtonRecall
+                    />}
+                    {recallAuth && <BpmButtonRecall
                         className="ml5 "
-                        checkedArray = {selectData}
-                        url = {`${GROBAL_HTTP_CTX}/sany_order/recall`}
-                        onSuccess = {this.onRecallSuc}
-                        onError = {this.onRecallFail}
-                        onStart = {this.onRecallStart}
-                    />
+                        checkedArray={selectData}
+                        url={`${GROBAL_HTTP_CTX}/sany_order/recall`}
+                        onSuccess={this.onRecallSuc}
+                        onError={this.onRecallFail}
+                        onStart={this.onRecallStart}
+                    />}
                 </div>
                 <PaginationTable
                     data={list}
@@ -252,7 +253,7 @@ export default class SimplePaginationTable extends Component {
                     onTableSelectedData={this.onTableSelectedData}
                     onPageSizeSelect={this.onPageSizeSelect}
                     onPageIndexSelect={this.onPageIndexSelect}
-                    scroll={{ x: 1550, y: 500}}
+                    scroll={{ x: 1550, y: 500 }}
                 />
             </div>
 
